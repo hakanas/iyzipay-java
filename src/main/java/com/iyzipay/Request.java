c604b4a3b56458d44a5e1a64b9ba24f989f1dfba
Burçin Aydoğmuş <burcin.aydogmus@iyzico.com>
2020-03-13 15:29:56 +0300

    Adds correlationId to base request

--- src/main/java/com/iyzipay/Request.java
+++ src/main/java/com/iyzipay/Request.java
@@ -4,6 +4,7 @@ public class Request {
 
     private String locale;
     private String conversationId;
+    private String correlationId;
 
     public String getLocale() {
         return locale;
@@ -21,11 +22,20 @@ public class Request {
         this.conversationId = conversationId;
     }
 
+    public String getCorrelationId() {
+        return correlationId;
+    }
+
+    public void setCorrelationId(String correlationId) {
+        this.correlationId = correlationId;
+    }
+
     @Override
     public String toString() {
         return new ToStringRequestBuilder(this)
                 .append("locale", locale)
                 .append("conversationId", conversationId)
+                .append("correlationId", correlationId)
                 .toString();
     }
 }
